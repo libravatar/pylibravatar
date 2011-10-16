b553112c0ba273d6d4bc94467aaab424d8dd4d43
Francois Marier <fmarier@gmail.com>
2011-10-16 20:30:49 +1300

    Mark this package as production/stable

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     requires = ['pydns'],
     classifiers = [
         "Programming Language :: Python",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
