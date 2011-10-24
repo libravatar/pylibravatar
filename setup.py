02fd2c5b55f811fa6dd63314cd571007c0c80038
Francois Marier <fmarier@gmail.com>
2011-10-24 21:13:26 +1300

    Bump changelog and version number for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'pyLibravatar',
-    version = '1.4',
+    version = '1.5',
     description = 'Python module for Libravatar',
     author = 'Francois Marier',
     author_email = 'francois@libravatar.org',
